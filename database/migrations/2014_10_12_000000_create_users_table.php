73671404491a7bed1eef6b0f82d69b6db526e80a
Amodeo Lorenzo <lrnzmd@gmail.com>
2021-08-21 11:03:37 +0200

    lezione 18

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,4 +1,4 @@
-<?php
+Post::findOrFail($slug)<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
