12c99989976cd6d01f0edb14c77b18be5bf72142
Amodeo Lorenzo <lrnzmd@gmail.com>
2021-08-22 12:31:29 +0200

    lezione 23

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
         @foreach ($posts as $post)
             <article class="{{ $loop-> even ? 'footbar' : ''}}">
                 <h1>
-                    <a href="/posts/{{ $post->id }}">
+                    <a href="/posts/{{ $post->slug }}">
                         {{ $post->title }}
                     </a>
                 </h1>
