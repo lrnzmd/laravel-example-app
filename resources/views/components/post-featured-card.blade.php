1dfd324053fb92fcd20e47f159b240d09a34977b
Amodeo Lorenzo <lrnzmd@gmail.com>
2021-08-23 11:44:36 +0200

    lezione 33

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -15,7 +15,7 @@
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        <a href="/posts/{{ $post->slug }}"{{ $post->title }}</a>
+                        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
@@ -33,7 +33,7 @@
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->author->name }}</h5>
                         <h6>Mascot at Laracasts</h6>
