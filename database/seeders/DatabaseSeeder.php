ac8844d909a9c4ac2f2e2035a0012c40e9a36bf4
Amodeo Lorenzo <lrnzmd@gmail.com>
2021-08-23 09:15:49 +0200

    lezione 29

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
 
 
-        User::factory(50)->create();
-        Category::factory(10)->create();
-        Post::factory(10)->create();
+        // User::factory(20)->create();
+        // Category::factory(10)->create();
+        // Post::factory(10)->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+        Post::factory()->create();
+
 
         // $personal = Category::create([
         //     'name' => 'Personal',
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
-
+use App\Models\User;
 
 
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get()
     ]);
 });
 
@@ -24,5 +24,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts'=> $category-> posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts'=> $author-> posts
     ]);
 });
