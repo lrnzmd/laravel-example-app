fe4d5c0ba467c4df2b2bebb24f34ba2b7c1c3971
Amodeo Lorenzo <lrnzmd@gmail.com>
2021-08-23 15:24:20 +0200

    lezione 36 e sezione 5

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -25,12 +25,10 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
-                <p class="mt-4">
-                    <strong>
-                        {{ $post->excerpt }}
-                    </strong>
-                </p>
+            <div class="text-sm mt-2 space-y-4">
+
+                        {!! $post->excerpt !!}
+
             </div>
 
 
