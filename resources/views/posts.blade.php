0399d15433ac0dc8057c23ba3e6f9fb226d39be9
Amodeo Lorenzo <lrnzmd@gmail.com>
2021-08-23 09:57:39 +0200

    lezione 31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,23 @@
 <x-layout>
-    <x-slot name="content">
-        @foreach ($posts as $post)
-            <article class="{{ $loop-> even ? 'footbar' : ''}}">
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
+    @include('_post-header')
 
-                <p>
-                By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-                </p>
 
-                <div>
-                    {!! $post->excerpt !!}
-                </div>
-            </article>
-        @endforeach
-    </x-slot>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        <x-post-featured-card />
+
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+
+        </div>
+    </main>
 </x-layout>
